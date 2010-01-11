a89daab3784a23fc50ea1d0ed6deab2e58d01b89
Jakub Kuźma <kuba@objectreload.(none)>
2010-01-11 11:18:12 +0100

    bumped version to 0.0.1

--- lib/magic.rb
+++ lib/magic.rb
@@ -6,4 +6,39 @@ require "magic/api"
 require "magic/constants"
 require "magic/database"
 
+module Magic
+  class << self
+    def guess_file_mime(file)
+      guess(:file, :mime, file)
+    end
 
+    def guess_file_mime_encoding(file)
+      guess(:file, :mime_encoding, file)
+    end
+
+    def guess_file_mime_type(file)
+      guess(:file, :mime_type, file)
+    end
+
+    def guess_string_mime(string)
+      guess(:buffer, :mime, string)
+    end
+
+    def guess_string_mime_encoding(string)
+      guess(:buffer, :mime_encoding, string)
+    end
+
+    def guess_string_mime_type(string)
+      guess(:buffer, :mime_type, string)
+    end
+
+    protected
+
+    def guess(type, what, where)
+      db = Database.new(what)
+      result = db.send(type, where)
+      db.close
+      result
+    end
+  end
+end
--- lib/magic/api.rb
+++ lib/magic/api.rb
@@ -7,13 +7,13 @@ module Magic
     attach_function :magic_open, [:int], :pointer
     attach_function :magic_close, [:pointer], :void
     attach_function :magic_file, [:pointer, :string], :pointer
-    attach_function :magic_descriptor, [:pointer, :int], :string
+    # attach_function :magic_descriptor, [:pointer, :int], :string
     attach_function :magic_buffer, [:pointer, :pointer, :size_t], :pointer
     attach_function :magic_error, [:pointer], :string
-    attach_function :magic_setflags, [:pointer, :int], :int
+    # attach_function :magic_setflags, [:pointer, :int], :int
     attach_function :magic_load, [:pointer, :string], :int
-    attach_function :magic_compile, [:pointer, :string], :int
-    attach_function :magic_check, [:pointer, :string], :int
-    attach_function :magic_errno, [:pointer], :int
+    # attach_function :magic_compile, [:pointer, :string], :int
+    # attach_function :magic_check, [:pointer, :string], :int
+    # attach_function :magic_errno, [:pointer], :int
   end
 end
