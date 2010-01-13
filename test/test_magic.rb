de13f2d8aea0845fbd5e5d813b58a295dec55408
Jakub Kuźma <qoobaa+github@gmail.com>
2010-01-13 09:44:39 +0100

    changed tests to declarative syntax

--- test/test_magic.rb
+++ test/test_magic.rb
@@ -1,57 +1,45 @@
 require 'helper'
 
 class TestMagic < Test::Unit::TestCase
-  def test_guess_magic_text_mime
+  test "guess magic.txt mime" do
     assert_equal "text/plain; charset=utf-8", Magic.guess_file_mime(fixture("magic.txt"))
   end
 
-  def test_guess_magic_text_mime_type
+  test "guess magic.txt mime type" do
     assert_equal "text/plain", Magic.guess_file_mime_type(fixture("magic.txt"))
   end
 
-  def test_guess_magic_text_mime_encoding
+  test "guess magic.txt mime encoding" do
     assert_equal "utf-8", Magic.guess_file_mime_encoding(fixture("magic.txt"))
   end
 
-  def test_guess_magic_logo_mime
+  test "guess filelogo.jpg mime" do
     assert_equal "image/jpeg; charset=binary", Magic.guess_file_mime(fixture("filelogo.jpg"))
   end
 
-  def test_guess_magic_logo_mime_type
+  test "guess filelogo.jpg mime type" do
     assert_equal "image/jpeg", Magic.guess_file_mime_type(fixture("filelogo.jpg"))
   end
 
-  def test_guess_magic_logo_mime_encoding
+  test "guess filelogo.jpg mime encoding" do
     assert_equal "binary", Magic.guess_file_mime_encoding(fixture("filelogo.jpg"))
   end
 
-  def test_guess_non_existing_file_mime
+  test "guess non-existing file mime" do
     assert_raises Magic::Exception do
       Magic.guess_file_mime(fixture("non-existing.file"))
     end
   end
 
-  def test_guess_non_existing_file_mime_type
-    assert_raises Magic::Exception do
-      Magic.guess_file_mime_type(fixture("non-existing.file"))
-    end
-  end
-
-  def test_guess_non_existing_file_mime_encoding
-    assert_raises Magic::Exception do
-      Magic.guess_file_mime_encoding(fixture("non-existing.file"))
-    end
-  end
-
-  def test_guess_magic_logo_mime_with_jpeg_database
+  test "guess filelogo.jpg mime with magic_jpeg database" do
     assert_equal "image/jpeg; charset=binary", Magic.guess_file_mime(fixture("filelogo.jpg"), :database => fixture("magic_jpeg"))
   end
 
-  def test_guess_magic_logo_mime_with_empty_database
+  test "guess filelogo.jpg mime with empty database" do
     assert_equal "application/octet-stream; charset=binary", Magic.guess_file_mime(fixture("filelogo.jpg"), :database => fixture("magic_empty"))
   end
 
-  def test_guess_with_block
+  test "guess with block" do
     result = nil
     Magic.guess(:mime) { |db| result = db.file(fixture("filelogo.jpg")) }
     assert_equal "image/jpeg; charset=binary", result
